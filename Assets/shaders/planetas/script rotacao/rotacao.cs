48ae847e08bd622a0e1d3d4c41b1f2f65bb49435
pietrogos <pietrogos@gmail.com>
2023-07-16 14:56:03 -0300

    mais planetas
    fiz mais planetas

---
+++ Assets/shaders/planetas/script rotacao/rotacao.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacao : MonoBehaviour
+{
+    public float speed = 10f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime, Space.World);
+    }
+}

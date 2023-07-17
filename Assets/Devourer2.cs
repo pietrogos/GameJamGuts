89c51da69eeea546cfca83bd565b57af4fb7d3b0
xarlosbrazil <133508168+xarlosbrazil@users.noreply.github.com>
2023-07-16 21:45:15 -0300

    sem grandes atualizacoes

--- Assets/Devourer2.cs
+++ Assets/Devourer2.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Devourer2 : MonoBehaviour
 {
+    public AudioSource somMordida;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,12 @@ public class Devourer2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    void OnTriggerEnter(Collider other)
+
+    {
+        somMordida.Play();
+
     }
 }
